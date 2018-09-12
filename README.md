ced52770a0ed95ef3dd8652d47d62e5f62094211
dubuc <gilles.dubuc@nuance.com>
2018-09-12 14:49:25 -0400

    Initial commit. Terraform files and README.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Terraform Azure MSI Example
+
+This will use Terraform to deploy a VM Scale Set with Managed Service Identity on Azure.
---
+++ main.tf
@@ -0,0 +1,136 @@
+resource "azurerm_resource_group" "test" {
+  name     = "${var.prefix}"
+  location = "East US 2"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "${var.prefix}-vn"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "${var.prefix}-sub"
+  resource_group_name  = "${azurerm_resource_group.test.name}"
+  virtual_network_name = "${azurerm_virtual_network.test.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_public_ip" "test" {
+  name                         = "${var.prefix}-pip"
+  location                     = "${azurerm_resource_group.test.location}"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  public_ip_address_allocation = "static"
+  domain_name_label            = "${azurerm_resource_group.test.name}"
+
+  tags {
+    environment = "test"
+  }
+}
+
+resource "azurerm_lb" "test" {
+  name                = "${var.prefix}-lb"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = "${azurerm_public_ip.test.id}"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  loadbalancer_id     = "${azurerm_lb.test.id}"
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_nat_pool" "lbnatpool" {
+  count                          = 3
+  resource_group_name            = "${azurerm_resource_group.test.name}"
+  name                           = "ssh"
+  loadbalancer_id                = "${azurerm_lb.test.id}"
+  protocol                       = "Tcp"
+  frontend_port_start            = 50000
+  frontend_port_end              = 50119
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_virtual_machine_scale_set" "test" {
+  name                = "${var.prefix}-scaleset-1"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name     = "Standard_DS1_v2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+    lun            = 0
+    caching        = "ReadWrite"
+    create_option  = "Empty"
+    disk_size_gb   = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "testvm"
+    admin_username       = "azureuser"
+    admin_password       = "Passwword1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/azureuser/.ssh/authorized_keys"
+      key_data = "${file("~/.ssh/id_rsa.pub")}"
+    }
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "${var.prefix}-IPConfiguration"
+      subnet_id                              = "${azurerm_subnet.test.id}"
+      load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.bpepool.id}"]
+      load_balancer_inbound_nat_rules_ids    = ["${element(azurerm_lb_nat_pool.lbnatpool.*.id, count.index)}"]
+    }
+  }
+
+  # This enables use of of SystemAssigned Identity
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags {
+    environment = "test"
+  }
+}
+
+# This is where the ScaleSet Managed Service Identity gets Resource Group "Reader" role
+resource "azurerm_role_assignment" "test" {
+  scope                = "${azurerm_resource_group.test.id}"
+  role_definition_name = "Reader"
+  principal_id         = "${lookup(azurerm_virtual_machine_scale_set.test.identity[0], "principal_id")}"
+}
